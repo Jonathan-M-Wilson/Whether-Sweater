8a0449f83ad5cc06cd409ff648aeecc17d65ee7c
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-18 09:32:14 -0700

    Update retrieve background image for city spec to reflect image poro changes

--- spec/requests/api/v1/backgrounds/retrieve_background_image_for_city_spec.rb
+++ spec/requests/api/v1/backgrounds/retrieve_background_image_for_city_spec.rb
@@ -25,38 +25,39 @@ RSpec.describe "Get Background Image for City Endpoint" do
     expect(background_json[:data][:type]).to eq('image')
     expect(background_json[:data]).to have_key(:attributes)
     expect(background_json[:data][:attributes].size).to eq(3)
-    expect(background_json[:data][:attributes]).to have_key(:credit)
-    expect(background_json[:data][:attributes]).to have_key(:location)
-    expect(background_json[:data][:attributes]).to have_key(:image_url)
-    expect(background_json[:data][:attributes][:credit].size).to eq(3)
-    expect(background_json[:data][:attributes][:credit]).to have_key(:source)
-    expect(background_json[:data][:attributes][:credit]).to have_key(:author)
-    expect(background_json[:data][:attributes][:credit]).to have_key(:logo)
+    expect(background_json[:data][:attributes]).to have_key(:image)
+    expect(background_json[:data][:attributes][:image]).to have_key(:credit)
+    expect(background_json[:data][:attributes][:image]).to have_key(:location)
+    expect(background_json[:data][:attributes][:image]).to have_key(:image_url)
+    expect(background_json[:data][:attributes][:image][:credit].size).to eq(3)
+    expect(background_json[:data][:attributes][:image][:credit]).to have_key(:source)
+    expect(background_json[:data][:attributes][:image][:credit]).to have_key(:author)
+    expect(background_json[:data][:attributes][:image][:credit]).to have_key(:logo)
 
     data_attributes = background_json[:data][:attributes]
-    data_attributes_credits = background_json[:data][:attributes][:credit]
+    data_attributes_credits = background_json[:data][:attributes][:image][:credit]
 
-    credit = background_json[:data][:attributes][:credit]
+    credit = background_json[:data][:attributes][:image][:credit]
     expect(credit).to be_a(Hash)
     expect(data_attributes[:credit]).to_not include(nil)
 
-    location = background_json[:data][:attributes][:location]
+    location = background_json[:data][:attributes][:image][:location]
     expect(location).to be_a(String)
     expect(data_attributes[:location]).to_not eq(nil)
 
-    image_url = background_json[:data][:attributes][:image_url]
+    image_url = background_json[:data][:attributes][:image][:image_url]
     expect(image_url).to be_a(String)
     expect(data_attributes[:image_url]).to_not eq(nil)
 
-    source = background_json[:data][:attributes][:credit][:source]
+    source = background_json[:data][:attributes][:image][:credit][:source]
     expect(source).to be_a(String)
     expect(data_attributes_credits[:source]).to_not eq(nil)
 
-    author = background_json[:data][:attributes][:credit][:author]
+    author = background_json[:data][:attributes][:image][:credit][:author]
     expect(author).to be_a(String)
     expect(data_attributes_credits[:author]).to_not eq(nil)
 
-    logo = background_json[:data][:attributes][:credit][:logo]
+    logo = background_json[:data][:attributes][:image][:credit][:logo]
     expect(logo).to be_a(String)
     expect(data_attributes_credits[:logo]).to_not eq(nil)
   end

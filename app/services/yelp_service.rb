d1045e8e2513739c775b61be83a4b15ee8c512b8
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-19 10:52:30 -0700

    Fix yelp service

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,9 +1,10 @@
 class YelpService
   class << self
-    def get_buisnesses(destination_city, unix_time)
-      response = conn.get "v3/businesses/search" do |req|
+    def get_businesses(destination_city, unix_time, food)
+      response = conn.get "/v3/businesses/search" do |req|
         req.params['location'] = destination_city
         req.params['open_at'] = unix_time
+        req.params['term'] = food
       end
       get_json(response)
     end
@@ -15,7 +16,7 @@ class YelpService
     end
 
     def conn
-      Faraday.new(url: 'https://api.yelp.com/') do |f|
+      Faraday.new(url: 'https://api.yelp.com') do |f|
       f.headers['Authorization'] = ENV['Yelp_API_KEY']
       end
     end

765ddf2f6dd5e4508d6ef8310e05b907b13434c2
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-18 18:16:11 -0700

    Create road trip poro with method to format the travel time

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,36 @@
+class RoadTrip
+  attr_reader :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
+
+  def initialize(data)
+    @start_city = data[:start_city]
+    @end_city = data[:end_city]
+    @travel_time = formatted_travel_time(data[:travel_time])
+    @weather_at_eta = data[:weather_at_eta]
+  end
+
+  def formatted_travel_time(data)
+    minutes = data.to_f / 60
+    hours = (minutes / 60).to_i
+    fractal_hours = (minutes / 60).round(2).modulo(1)
+    fractal_minutes = (fractal_hours * 60).round
+
+    pluralize_hours =
+      if hours == 1
+        "hour"
+      else
+        "hours"
+      end
+
+    pluralize_minutes =
+      if fractal_minutes == 1
+        "minute"
+      else
+        "minutes"
+      end
+
+    "#{hours} #{pluralize_hours}, #{fractal_minutes} #{pluralize_minutes}"
+  end
+end

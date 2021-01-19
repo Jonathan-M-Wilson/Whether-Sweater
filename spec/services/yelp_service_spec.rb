237ceddffb383ce7a36beef8d2fa6048811dc85c
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-19 10:29:49 -0700

    Update service spec, as there is no chinese food in Pueblo

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Yelp Service' do
   it "can retrieve name and address of a restaurant in your end location that is open" do
-    #VCR.use_cassette('/Yelp_Service/can_retrieve_restaurant_data') do
-      end_city = 'Pueblo,Co'
+    VCR.use_cassette('/Yelp_Service/can_retrieve_restaurant_data') do
+      end_city = 'Denver,Co'
       unix_time = 1610955000
-      food = 'burger'
+      food = 'chinese'
 
       restaurant_data = YelpService.get_buisnesses(end_city, unix_time, food)
-
       first_resaurant = restaurant_data[:businesses][0]
-
-      require "pry"; binding.pry
 
       expect(first_resaurant[:name]).to be_a(String)
       expect(first_resaurant[:is_closed]).to eq(false)
       expect(first_resaurant[:location][:display_address]).to be_an(Array)
     end
+  end 
 end

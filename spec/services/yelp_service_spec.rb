090d40073ab0134d5cfdfca4328cea79ef60e82f
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-19 09:51:39 -0700

    Work on passing a term param

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Yelp Service' do
-  it "can retrieve name and address of a restaurant in your end location that is open", :vcr do
-    VCR.use_cassette('/Yelp_Service/can_retrieve_restaurant_data') do
+  it "can retrieve name and address of a restaurant in your end location that is open" do
+    #VCR.use_cassette('/Yelp_Service/can_retrieve_restaurant_data') do
       end_city = 'Pueblo,Co'
       unix_time = 1610955000
+      food = 'burger'
 
-      restaurant_data = YelpService.get_buisnesses(end_city, unix_time)
+      restaurant_data = YelpService.get_buisnesses(end_city, unix_time, food)
 
       first_resaurant = restaurant_data[:businesses][0]
+
+      require "pry"; binding.pry
 
       expect(first_resaurant[:name]).to be_a(String)
       expect(first_resaurant[:is_closed]).to eq(false)
       expect(first_resaurant[:location][:display_address]).to be_an(Array)
     end
-  end
 end

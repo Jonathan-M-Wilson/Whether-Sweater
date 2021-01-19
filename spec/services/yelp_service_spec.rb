c3ac689ae5cd6ceb7eab553bb2c722e3b411cbaa
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-01-19 09:37:50 -0700

    Finish rough draft of service spec and add fixture data

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Yelp Service' do
-  it "can retrieve name and address of a restaurant in your end location", :vcr do
+  it "can retrieve name and address of a restaurant in your end location that is open", :vcr do
     VCR.use_cassette('/Yelp_Service/can_retrieve_restaurant_data') do
       end_city = 'Pueblo,Co'
       unix_time = 1610955000
@@ -10,8 +10,9 @@ RSpec.describe 'Yelp Service' do
 
       first_resaurant = restaurant_data[:businesses][0]
 
-      expect(restaurant_data[:])
-      require "pry"; binding.pry
+      expect(first_resaurant[:name]).to be_a(String)
+      expect(first_resaurant[:is_closed]).to eq(false)
+      expect(first_resaurant[:location][:display_address]).to be_an(Array)
     end
   end
 end
